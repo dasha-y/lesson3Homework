a755c358a6e18f4339e685f6fb8ccf673a8f1467
Dasha <dsaluk@gmail.com>
2023-11-12 15:30:12 +0300

    new branch message

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("активно делаем дз");
         System.out.println("Все еще активно делаем дз");
+
+
+        System.out.println("Инфа для новой ветки");
+
+
     }
 }
