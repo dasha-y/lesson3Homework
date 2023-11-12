fdc209224cd0e49a1f9122e2f50af163da9cede7
Dasha <dsaluk@gmail.com>
2023-11-12 15:20:42 +0300

    new message

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("активно делаем дз");
+        System.out.println("Все еще активно делаем дз");
     }
 }
